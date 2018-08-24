3697e01d06e86013f6138d8aae852968f52a8bdc
Gurpreet Singh <gs838s@att.com>
2018-08-24 16:49:19 +0530

    Create cli
    Create cli and introduce options to generate intermediary file
    and manifests file

--- tugboat/tugboat.py
+++ tugboat/tugboat.py
@@ -15,6 +15,8 @@
 import sys
 import getopt
 
+import click
+
 from tugboat.parser_engine.generate_intermediary import GenerateYamlFromExcel
 
 from tugboat.site_processors.baremetal_processor import BaremetalProcessor
@@ -32,34 +34,83 @@ processors = [
 ]
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    # parse command line options
-    filename_path = ''
-    excel_spec_path = ''
+def generate_intermediary_file(excel, spec, both=None):
+    """ Generate intermediary file """
+    if excel and spec:
+        parser = GenerateYamlFromExcel(excel, spec)
+        intermediary = parser.generate_yaml()
+        print('Generating intermediary file {}'.format(intermediary))
+    else:
+        print('Please pass engineering excel and spec file')
+    if both:
+        return intermediary
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'h', 'help')
-    except getopt.GetoptError as msg:
-        print('for help use --help')
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            print('Please provide xls and excelspec files as follows: python '
-                  'tugboat.py <spreadsheet> <excelspec>')
-            sys.exit(0)
 
-    # for arg in args:
-    filename_path = args[0]
-    excel_spec_path = args[1]
+def generate_manifest_files(intermediary):
+    """ Generate manifests """
+    if intermediary:
+        print('Generating manifest files')
+        for processor in processors:
+            processor_engine = processor(intermediary)
+            processor_engine.render_template()
+    else:
+        print('Please pass intermediary file')
+
+
+@click.command()
+@click.option(
+    '--generate_intermediary', '-g', is_flag=True,
+    help='Generate intermediary file from passed excel and excel spec'
+)
+@click.option(
+    '--generate_manifests', '-m', is_flag=True,
+    help='Generate manifests from the generated intermediary file'
+)
+@click.option(
+    '--excel', '-x', type=click.Path(exists=True),
+    help='Path to engineering excel file, to be passed with '
+    'generate_intermediary'
+)
+@click.option(
+    '--spec', '-s', type=click.Path(exists=True),
+    help='Path to excel spec, to be passed with generate_intermediary'
+)
+@click.option(
+    '--intermediary', '-i', type=click.Path(exists=True),
+    help='Path to intermediary file, to be passed with generate_manifests'
+)
+def main(generate_intermediary, generate_manifests, excel, spec, intermediary):
+    """ Generate intermediary and manifests files """
+    # if argv is None:
+    #     argv = sys.argv
+    # # parse command line options
+    # filename_path = ''
+    # excel_spec_path = ''
+    #
+    # try:
+    #     opts, args = getopt.getopt(sys.argv[1:], 'h', 'help')
+    # except getopt.GetoptError as msg:
+    #     print('for help use --help')
+    # for o, a in opts:
+    #     if o in ('-h', '--help'):
+    #         print('Please provide xls and excelspec files as follows: python'
+    #               'tugboat.py <spreadsheet> <excelspec>')
+    #         sys.exit(0)
+    #
+    # # for arg in args:
+    # filename_path = args[0]
+    # excel_spec_path = args[1]
 
     # Generate YAML from Excel Workbook engine
-    parser = GenerateYamlFromExcel(filename_path, excel_spec_path)
-    intermediary = parser.generate_yaml()
+    if generate_intermediary and generate_manifests:
+        intermediary = generate_intermediary_file(excel, spec, both=True)
+        generate_manifest_files(intermediary)
 
-    for processor in processors:
-        processor_engine = processor(intermediary)
-        processor_engine.render_template()
+    elif generate_intermediary:
+        generate_intermediary_file(excel, spec)
+
+    elif generate_manifests:
+        generate_manifest_files(intermediary)
 
 
 if __name__ == '__main__':
