b0cb6a12e93be1b7561e964b253210ebc4d2e79a
purnendu ghosh <purnendu@gmail.com>
2018-09-11 17:52:50 +0530

    Added support for ldap_username to be configured and additional updates according to working site

--- tugboat/site_processors/software_processor.py
+++ tugboat/site_processors/software_processor.py
@@ -20,15 +20,17 @@ import pprint
 
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
-
+from .base import BaseProcessor
 
 class SoftwareProcessor:
     def __init__(self, file_name):
+        BaseProcessor.__init__(self, file_name)
         self.logger = logging.getLogger(__name__)
         raw_data = self.read_file(file_name)
         yaml_data = self.get_yaml_data(raw_data)
         self.data = yaml_data
         self.dir_name = yaml_data['region_name']
+        self.data['ldap_username'] = self.rules_data['ldap_username']
 
     @staticmethod
     def read_file(file_name):
