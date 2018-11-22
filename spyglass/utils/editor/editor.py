be967db053d35d1890069cdbd6a35d576f5bb4bb
Pawan Singh Pal <pawansingh126@gmail.com>
2018-11-22 22:14:54 +0530

    Add logging to yaml-editor.

--- spyglass/utils/editor/editor.py
+++ spyglass/utils/editor/editor.py
@@ -15,26 +15,34 @@
 
 import click
 import json
+import logging
 import os
 import socket
 import sys
 import yaml
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from flask_bootstrap import Bootstrap
 
 
 app_path = os.path.dirname(os.path.abspath(__file__))
-print(app_path, os.getcwd())
 app = Flask('Yaml Editor!',
             template_folder=os.path.join(app_path, 'templates'),
             static_folder=os.path.join(app_path, 'static'))
 Bootstrap(app)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+LOG = app.logger
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Renders index page to edit provided yaml file."""
+    LOG.info('Rendering yaml file for editing')
     with open(app.config['YAML_FILE']) as file_obj:
         data = yaml.safe_load(file_obj)
     return render_template('yaml.html',
@@ -45,6 +53,7 @@ def index():
 @app.route('/save', methods=['POST'])
 def save():
     """Save current progress on file."""
+    LOG.info('Saving edited inputs from user to yaml file')
     out = request.json.get('yaml_data')
     with open(app.config['YAML_FILE'], 'w') as file_obj:
         yaml.safe_dump(out, file_obj, default_flow_style=False)
@@ -54,6 +63,8 @@ def save():
 @app.route('/saveExit', methods=['POST'])
 def save_exit():
     """Save current progress on file and shuts down the server."""
+    LOG.info('Saving edited inputs from user to yaml file and shutting'
+             ' down server')
     out = request.json.get('yaml_data')
     with open(app.config['YAML_FILE'], 'w') as file_obj:
         yaml.safe_dump(out, file_obj, default_flow_style=False)
@@ -66,11 +77,13 @@ def save_exit():
 @app.errorhandler(404)
 def page_not_found(e):
     """Serves 404 error."""
+    LOG.info('User tried to access unavailable page.')
     return '<h1>404: Page not Found!</h1>'
 
 
 def run(*args, **kwargs):
     """Starts the server."""
+    LOG.info('Initiating web server for yaml editing')
     port = kwargs.get('port', None)
     if not port:
         port = 8161
@@ -103,16 +116,33 @@ def run(*args, **kwargs):
     help="Text which is required to be changed on yaml file."
 )
 def main(*args, **kwargs):
+    LOG.setLevel(logging.INFO)
+    LOG.info('Initiating yaml-editor')
     try:
-        yaml.load(kwargs['file'], Loader=yaml.Loader)
-    except Exception:
-        print('Please provide a valid yaml file.')
+        yaml.safe_load(kwargs['file'])
+    except yaml.YAMLError as e:
+        LOG.error('EXITTING - Please provide a valid yaml file.')
+        if hasattr(e, 'problem_mark'):
+            mark = e.problem_mark
+            LOG.error("Error position: ({0}:{1})".format(
+                mark.line + 1, mark.column + 1))
         sys.exit(2)
-    print("Please go to http://{0}:{1}/ to edit your yaml file.".format(
+    except Exception:
+        LOG.error('EXITTING - Please provide a valid yaml file.')
+        sys.exit(2)
+    LOG.info("""
+
+##############################################################################
+
+Please go to http://{0}:{1}/ to edit your yaml file.
+
+##############################################################################
+
+    """.format(
         socket.gethostbyname(socket.gethostname()), kwargs['port']))
     app.config['YAML_FILE'] = kwargs['file'].name
     app.config['STRING_TO_CHANGE'] = kwargs['string']
-    run(kwargs)
+    run(*args, **kwargs)
 
 
 if __name__ == '__main__':
